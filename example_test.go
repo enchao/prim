da735dd51ad0921af1c7be6c66e446fb61701dea
enchao <shawreade@qq.com>
2016-01-10 22:33:56 +0800

    anatomy of a Go project

---
+++ example_test.go
@@ -0,0 +1,17 @@
+package prime_test
+
+import (
+	"fmt"
+
+	"github.com/enchao/prime"
+)
+
+func ExampleIsPrime() {
+	var candidate uint64 = 5
+	if prime.IsPrime(candidate) {
+		fmt.Println("Five is prime.")
+	} else {
+		fmt.Println("Five is not prime.")
+	}
+	// Output: Five is prime.
+}
